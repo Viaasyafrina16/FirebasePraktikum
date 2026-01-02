d4c3ba816ec250743f0e59feca9395ebb05f0312
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-02 17:45:01 +0700

    Menambahkan DestinasiHome sebagai rute navigasi halaman Home

---
+++ app/src/main/java/com/example/firebasepraktikum/view/Route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.firebasepraktikum.view.Route
+
+import com.example.firebasepraktikum.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
