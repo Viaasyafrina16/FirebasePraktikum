c112c19c0ee78c1d3c16cc38b7fe3d48389f3565
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-08 08:37:04 +0700

    mengambil data lengkap siswa dari database (repositorySiswa) berdasarkan ID

--- app/src/main/java/com/example/firebasepraktikum/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebasepraktikum/viewmodel/EditViewModel.kt
@@ -1,1 +1,31 @@
 package com.example.firebasepraktikum.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasepraktikum.ModelData.UIStateSiswa
+import com.example.firebasepraktikum.ModelData.toUiStateSiswa
+import com.example.firebasepraktikum.Repository.RepositorySiswa
+import com.example.firebasepraktikum.view.Route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+}
