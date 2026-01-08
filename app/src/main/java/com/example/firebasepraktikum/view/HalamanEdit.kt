884552e5b45ad907b31ed99c2e19eac672b78fe7
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-08 09:13:11 +0700

    menambah import

--- app/src/main/java/com/example/firebasepraktikum/view/HalamanEdit.kt
+++ app/src/main/java/com/example/firebasepraktikum/view/HalamanEdit.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebasepraktikum.view.Route.DestinasiEdit
+import com.example.firebasepraktikum.viewmodel.EditViewModel
 import com.example.firebasepraktikum.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
