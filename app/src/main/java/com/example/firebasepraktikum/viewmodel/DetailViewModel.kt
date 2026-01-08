b6e3e553c37a7b3018e72eac85bb97cd0b0bcd73
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-08 07:40:58 +0700

    mendefinisikan tiga kemungkinan status tampilan: Loading (sedang memuat), Success (berhasil mengambil data Siswa), atau Error

--- app/src/main/java/com/example/firebasepraktikum/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebasepraktikum/viewmodel/DetailViewModel.kt
@@ -1,1 +1,6 @@
 package com.example.firebasepraktikum.viewmodel
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object loading : StatusUIDetail
+}
