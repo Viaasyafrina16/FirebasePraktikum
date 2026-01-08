9fb9d02a26d21182cb9b9bf350ddf53b6842aa00
Viaasyafrina16 <syafrinavia@gmail.com>
2026-01-08 07:42:25 +0700

    mengambil ID siswa dari Navigation Arguments

--- app/src/main/java/com/example/firebasepraktikum/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebasepraktikum/viewmodel/DetailViewModel.kt
@@ -4,3 +4,15 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
     object loading : StatusUIDetail
 }
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa):ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
